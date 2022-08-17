fa40b4c65f967e8499aab0c4a6a72a1fa55f4044
john carlos tumal <johnc42311@gmail.com>
2022-08-16 21:43:05 -0500

    desafio seis

---
+++ script.js
@@ -0,0 +1,45 @@
+class Usuario {
+    constructor(nombre, numero, email){
+        this.nombre = nombre
+        this.numero = numero
+        this.email = email
+    }
+}
+
+
+const formulario = document.getElementById("formulario")
+const botonReserva= document.getElementById("botonReserva")
+const contenedorUno = document.getElementById("contenedorUno")
+
+const usuarios = []
+
+formulario.addEventListener('submit', (evento) =>{
+    evento.preventDefault()
+    const nombre = document.getElementById("nombre").value
+    const numero = document.getElementById("numero").value
+    const email = document.getElementById("email").value
+
+    const usuarioN = new Usuario (nombre, numero, email)
+
+    usuarios.push(usuarioN)
+
+    formulario.reset()
+    alert("Reserva Exitosa")
+    console.log(usuarios)
+
+})
+
+botonReserva.addEventListener('click', () =>{
+    contenedorUno.innerHTML = ""
+    usuarios.forEach((usuarioN, indice) => {
+        contenedorUno.innerHTML += `
+        <div class="card" id="user${indice}" style="width: 18rem;margin:10px;">
+                <div class="card-body">
+                    <h5 class="card-title">${usuarioN.nombre}</h5>
+                    <p class="card-text">${usuarioN.email}</p>
+                </div>
+            </div>
+        
+        `
+    })
+})
